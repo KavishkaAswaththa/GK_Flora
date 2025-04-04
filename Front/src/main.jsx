60593d92f74737c999422d2da744d7d3ff4fc18a
Meshani <msshani1209@gmail.com>
2025-04-04 14:06:16 +0530

    Profile day1

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-//import './styles/index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { AppContextProvider } from './context/Profile/AppContext.jsx';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <AppContextProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </AppContextProvider>
+  </BrowserRouter>,
+);
